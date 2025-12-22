8bdc7022b4fe3535289338d7f6b213288f8ae0db
danilaJDev <danila.rebkovets20@gmail.com>
2025-12-22 18:54:28 +0300

    100%

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,12 +1,17 @@
-export function mustGetEnv(name: string): string {
-    const value = process.env[name];
+const ensureEnv = (name: string, value: string | undefined): string => {
     if (!value) {
         throw new Error(`Missing environment variable: ${name}`);
     }
     return value;
-}
+};
 
 export const env = {
-    NEXT_PUBLIC_SUPABASE_URL: mustGetEnv("NEXT_PUBLIC_SUPABASE_URL"),
-    NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: mustGetEnv("NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY"),
+    NEXT_PUBLIC_SUPABASE_URL: ensureEnv(
+        "NEXT_PUBLIC_SUPABASE_URL",
+        process.env.NEXT_PUBLIC_SUPABASE_URL),
+
+    NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: ensureEnv(
+        "NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY",
+        process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
+    ),
 };
